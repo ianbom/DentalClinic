4c5874c3163d3592593a1acd35a7ad9ce02e0062
Ian Ale <ianalebom@gmail.com>
2025-12-30 20:05:45 +0700

    first dev

---
+++ public/build/assets/PatientLayout-DBNYWnVE.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-t53NlGWA.js";import{N as s,F as t}from"./Navbar-C7aHu4Z_.js";function i({children:o}){return r.jsxs(r.Fragment,{children:[r.jsx(s,{}),o,r.jsx(t,{})]})}export{i as P};
---
+++ public/build/assets/layout-ihZVVRdU.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-t53NlGWA.js";import{N as o,F as s}from"./Navbar-C7aHu4Z_.js";function i({children:t}){return r.jsxs(r.Fragment,{children:[r.jsx(o,{}),t,r.jsx(s,{})]})}export{i as default};
