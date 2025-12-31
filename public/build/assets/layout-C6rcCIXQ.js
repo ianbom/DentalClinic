57ced4c38de24ffb3343f40af70aa01313750658
Ian Ale <ianalebom@gmail.com>
2026-01-01 03:36:29 +0700

    fix design pasien

--- public/build/assets/layout-C6rcCIXQ.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-CNytDeoX.js";import{N as o,F as s}from"./Navbar-DRswFLY_.js";function i({children:t}){return r.jsxs(r.Fragment,{children:[r.jsx(o,{}),t,r.jsx(s,{})]})}export{i as default};
--- public/build/assets/layout-DDza7i0q.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-CNytDeoX.js";import{B as t}from"./BookingContext-BGvrRjar.js";function s({children:o}){return r.jsx(t,{children:o})}export{s as default};
