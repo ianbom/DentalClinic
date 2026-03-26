91165fb182c25139953b38ca15e65299167f20e2
Ian Ale <ianalebom@gmail.com>
2026-03-26 10:04:57 +0700

    ubah factory seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,12 +15,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
 
         $this->call([
             ProvinceSeeder::class,
@@ -28,7 +22,7 @@ class DatabaseSeeder extends Seeder
             DistrictSeeder::class,
             VillageSeeder::class,
             DoctorSeeder::class,
-            DoctorWorkingPeriodSeeder::class,
+            DoctorWorkingPeriodSeeder::class,   
             AdminSeeder::class,
             // BookingSeeder::class,
         ]);
